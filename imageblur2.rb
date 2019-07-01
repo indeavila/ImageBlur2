4d928da36ae6ef3e0facebb04b34bd93268d2d94
Ivan Nikolai De Avila <indeavila@gmail.com>
2019-07-01 20:42:03 +0000

    20190701340

--- imageblur2.rb
+++ imageblur2.rb
@@ -3,41 +3,34 @@ class Image
     @image = array
   end
   def output_image
-    @new_image.each_with_index do |r|
+    @image.each_with_index do |r|
       puts r.join
     end
   end
-  def blur(row, col)
+  def blur(row, col, new_image)
     if @image[row][col] == 1
-      @new_image[row][col] = 1
-      if row + 1 < @image.count
-        @new_image[row + 1][col] = 1
-      end
-      if row - 1 >= 0
-        @new_image[row - 1][col] = 1
-      end
-      if col + 1 < @image[row].count
-        @new_image[row][col + 1] = 1
-      end
-      if col -1 >= 0
-        @new_image[row][col - 1] = 1
-      end
+      new_image[row][col] = 1
+      new_image[row + 1][col] = 1 if row + 1 < @image.count
+      new_image[row - 1][col] = 1 if row - 1 >= 0
+      new_image[row][col + 1] = 1 if col + 1 < @image[row].count
+      new_image[row][col - 1] = 1 if col -1 >= 0
     end
   end
   def blur_loop
-    @new_image = []
+    new_image = []
     @image.each do |row|
       points = []
       row.each do |point|
         points << point
       end
-      @new_image << points
+      new_image << points
     end
       @image.each_with_index do |r, row|
         r.each_with_index do |c, col|
-          blur(row, col)
+          blur(row, col, new_image)
         end
       end
+      @image = new_image
   end
 end
 image = Image.new([
